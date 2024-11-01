13303e53933d01b0ac1bdbfe6e55b8ca5caad49c
LeemoTseng-Mac <leemotseng@LeeMo-Tseng.local>
2024-11-01 14:57:18 +0800

    init angular project

---
+++ src/app/homeview/homeview.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homeview',
+  standalone: true,
+  imports: [],
+  templateUrl: './homeview.component.html',
+  styleUrl: './homeview.component.css'
+})
+export class HomeviewComponent {
+
+}
