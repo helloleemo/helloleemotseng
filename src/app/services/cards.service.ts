0df9b37b86172d95437ccd9d11c637e078a55fef
LeemoTseng-Mac <leemotseng@LeeMo-Tseng.local>
2024-12-03 08:39:58 +0800

    update style

--- src/app/services/cards.service.ts
+++ src/app/services/cards.service.ts
@@ -23,7 +23,7 @@ export class CardsService {
         name: 'Product',
         description: 'description1',
         linkto: '',
-        img: 'https://picsum.photos/id/684/600/400',
+        img: 'https://picsum.photos/600/400',
         innerHtml: '<div class="bg-dark/10 min-w-full min-h-full rounded-lg">d</div>'
       }, {
         id: 2,
