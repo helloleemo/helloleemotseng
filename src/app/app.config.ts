b2d4df38d0b5b2a23c93d477ce550e54b49d09dd
helloleemo <leemo.tseng@gmail.com>
2024-12-11 21:34:14 +0800

    update hashUrl

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
@@ -9,10 +9,11 @@ import { MouseService } from './services/mouse.service';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes,withHashLocation()),
     provideAnimationsAsync(),
     provideHttpClient(),
     MouseService,
     provideHttpClient(),
+
   ]
 };
