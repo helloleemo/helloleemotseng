ca5ff55ec12dd56ff51856013088af65280b7e7a
LeemoTseng-Mac <leemotseng@LeeMo-Tseng.local>
2024-12-12 11:03:58 +0800

    update to-do drag and drop

--- src/app/cards/to-do-list/to-do-list.component.ts
+++ src/app/cards/to-do-list/to-do-list.component.ts
@@ -13,38 +13,72 @@ interface Todo {
 @Component({
   selector: 'app-to-do-list',
   standalone: true,
-  imports: [CommonModule, FormsModule, MatIconModule,TemplateComponent],
+  imports: [CommonModule, FormsModule, MatIconModule, TemplateComponent],
   templateUrl: './to-do-list.component.html',
 })
 export class ToDoListComponent {
-  newTodo = '';
+  newItem = '';
   currentColor = 'bg-gray-400';
   colors = ['bg-gray-400', 'bg-blue-400', 'bg-red-400', 'bg-green-400'];
-  todos: Todo[] = [];
-  private colorIndex = 0;
+  todolist: Todo[] = [];
+  colorIndex = 0;
+  isError = false;
 
-  onAddTodo(): void {
-    const trimmedText = this.newTodo.trim();
+  dragIndex: number | null = null;
+
+
+
+  // Add or remove items from the list
+  onInputChange(event: Event): void {
+    //: void - Not return any value.
+    this.newItem = (event.target as HTMLInputElement).value;
+    this.isError = false;
+  }
+  AddItem(): void {
+    // console.log(this.newItem);
+    const trimmedText = this.newItem.trim();
     if (trimmedText) {
-      this.todos.push({
+      this.todolist.push({
         text: trimmedText,
         completed: false,
         color: this.currentColor,
-      });
-      this.newTodo = '';
+      }
+      );
+      this.newItem = '';
+    } else {
+      this.isError = true;
     }
   }
-
   onToggleComplete(index: number): void {
-    this.todos[index].completed = !this.todos[index].completed;
+    this.todolist[index].completed = !this.todolist[index].completed;
   }
-
   onRemoveTodo(index: number): void {
-    this.todos.splice(index, 1);
+    this.todolist.splice(index, 1);
   }
-
   onChangeColor(): void {
     this.colorIndex = (this.colorIndex + 1) % this.colors.length;
     this.currentColor = this.colors[this.colorIndex];
   }
+
+
+  // Drag and Drop API
+
+  dragStart(e:DragEvent, i: number): void {
+    this.dragIndex = i;
+    e.dataTransfer?.setData('text/plain', i.toString());
+  }
+  dragging(e: Event): void {
+    e.preventDefault();
+  }
+  drop(e: Event, i: number): void {
+    e.preventDefault();
+    const draggedIndex = this.dragIndex;
+    if (draggedIndex !== null && draggedIndex !== i) {
+      const draggedItem = this.todolist[draggedIndex];
+      this.todolist.splice(draggedIndex, 1);
+      this.todolist.splice(i, 0, draggedItem);
+      this.dragIndex = null;
+    }
+  }
+
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,14 +1,14 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { environment } from '../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherService {
 
-  private apiUrl = 'environment.apiUrl';
-  private apiKey = 'environment.apiKey';
+  private apiUrl = `${environment.apiUrl}`;
+  private apiKey = `${environment.apiUrl}`;
 
   constructor(private http: HttpClient) { }
 
