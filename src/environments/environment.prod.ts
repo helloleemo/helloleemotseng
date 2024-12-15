43bfbd1a2820dc448ca7ef74f0e699fc75a628e4
helloleemo <leemo.tseng@gmail.com>
2024-12-15 21:42:53 +0800

    Update .gitignore to ignore environments files

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: "WEATHER_API_KEY",
-  apiUrl: 'https://opendata.cwa.gov.tw/api/v1/rest/datastore',
-  milestoneApiUrl: `json/milestones.json`,
-  flightApiUrl: `json/flights.json`,
-  fileApiUrl: `json/files.json`
-};
